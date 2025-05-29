1825a2fbcafca14fef517ee869bc72952def2228
TaufiqOM <pt.om.taufiq@gmail.com>
2025-05-29 08:23:39 +0700

    validasi

--- ITCustom_Barcode/wizard/barcode_wizard_line.py
+++ ITCustom_Barcode/wizard/barcode_wizard_line.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class BarcodeProduksiWizardLine(models.TransientModel):
     _name = 'barcode.produksi.wizard.line'
@@ -9,4 +10,10 @@ class BarcodeProduksiWizardLine(models.TransientModel):
     product_template_id = fields.Many2one(related='produksi_line_id.product_template_id', store=True, readonly=True)
     product_uom_qty = fields.Float(related='produksi_line_id.product_uom_qty', store=True, readonly=True)
     jumlah_generate = fields.Integer(string='Jumlah Generate', default=1)
+    belum_generate = fields.Integer(related='produksi_line_id.belum_generate', string='Belum Generate', store=False, readonly=True)
 
+    @api.onchange('jumlah_generate')
+    def _onchange_jumlah_generate(self):
+        for record in self:
+            if record.jumlah_generate > record.produksi_line_id.belum_generate:
+                raise UserError("Jumlah Generate Melebihi Stock")
